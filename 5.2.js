ad710a187418ab291647fb66dece6e15e05683f2
Melissa <mactechy@gmail.com>
2016-02-21 21:35:51 -0500

    added code samples

---
+++ 5.2.js
@@ -0,0 +1,11 @@
+var steps = ["brainstorm", "narrow", "prototype", "test", "propose"];
+steps.forEach(function(item) {
+  console.log(item);
+});
+// "brainstorm"
+// "narrow"
+// "prototype"
+// "test"
+// "propose"
+
+// http://jsbin.com/nixuzefive/edit?js,console
