8b03ce7e4a1f2910207344c0e5c7ecdc17f5b809
Melissa <mactechy@gmail.com>
2016-02-21 21:32:18 -0500

    added code samples

---
+++ 4.2.3.js
@@ -0,0 +1,12 @@
+var steps = ["brainstorm", "narrow", "prototype", "test", "propose"];
+console.log(steps.length); //5
+
+steps.length = 10;
+console.log(steps.length); //10
+console.log(steps[6]); //undefined
+
+steps.length = 3;
+console.log(steps[4]); //undefined
+console.log(steps); //["brainstorm", "narrow", "prototype"]
+
+http://jsbin.com/lezadeguci/edit?js,console
---
+++ 4.2.js
@@ -0,0 +1,22 @@
+var steps = ["brainstorm", "narrow", "prototype", "test", "propose"];
+var stepTerm;
+for(var counter = 0; counter < steps.length; counter++) {
+  switch(counter) {
+    case 0:
+      stepTerm = "First";
+      break;
+    case steps.length - 1:
+      stepTerm = "Finally";
+      break;
+    default:
+      stepTerm = "Then";
+  }
+  console.log(stepTerm + " we " + steps[counter]);
+}
+//"First we brainstorm"
+//"Then we narrow"
+//"Then we prototype"
+//"Then we test"
+//"Finally we propose"
+
+http://jsbin.com/xakudetemo/edit?js,console
