eca82ff4392bb6a90a0a7d389b39a7e405b15f56
Melissa <mactechy@gmail.com>
2016-02-22 21:43:59 -0500

    added code samples

--- 2.5.js
+++
@@ -1,7 +0,0 @@
-var source = "The kittens have mittens.";
-var vowelMatches = /[aeiou]/;
-console.log(source.match(vowelMatches)); //["e"]
-
-console.log(source.search(vowelMatches)); //2
-
-// http://jsbin.com/qekecatadu/edit?js,console
