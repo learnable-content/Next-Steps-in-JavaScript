ad09d712bcf1febcd8fd64d05d80ed5d5ebd3d00
Melissa <mactechy@gmail.com>
2016-02-22 21:39:06 -0500

    added code samples

--- 2.4.js
+++
@@ -1,8 +0,0 @@
-var source = "The kittens have mittens";
-var itMatchesFirst = /^it/;
-console.log(itMatchesFirst.test(source)); //false
-
-var theMatchesFirst = /^the/i;
-console.log(theMatchesFirst.test(source)); //true
-
-// http://jsbin.com/hehevufexa/edit?js,console
