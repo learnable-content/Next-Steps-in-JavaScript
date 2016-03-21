64c6f94628302481ef6fead9f32c7e379b7405ca
Bruno Mota <bruno12mota@gmail.com>
2016-03-21 16:07:03 +0000

    added a note on the comment on the .flags property

--- 2.5.4.js
+++ 2.5.4.js
@@ -4,7 +4,7 @@ console.log(vowelMatch.source); //"[aeiou]"
 var globalVowelMatch = /[aeiou]/g;
 console.log(globalVowelMatch.source); //"[aeiou]"
 
-console.log(globalVowelMatch.flags); //undefined
+console.log(globalVowelMatch.flags); //undefined or "g" (browser dependent)
 console.log(globalVowelMatch.toString()); //"/[aeiou]/g"
 console.log(globalVowelMatch.toString().match(/[gimuy]*$/)[0]); //"g"
 
