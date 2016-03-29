d45ec02426871c6cb29351cf878346a29b4b042c
M. David Green <mdavidgreen@users.noreply.github.com>
2016-03-29 13:44:24 -0700

    Update 3.3.js

---
+++ 3.3.js
@@ -0,0 +1,15 @@
+var counter;
+var functions = [];
+var values = [];
+for(counter = 0; counter < 5; counter++) {
+  values[counter] = counter;
+  functions[counter] = function() {
+    return counter;
+  };
+}
+console.log(values[0]); //0
+console.log(values[2]); //2
+console.log(values[4]); //4
+console.log(functions[0]()); //5
+console.log(functions[2]()); //5
+console.log(functions[4]()); //5
